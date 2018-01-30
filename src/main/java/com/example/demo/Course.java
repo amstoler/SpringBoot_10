6c72173e16ad5100c271a4dcefb200ff7bac7342
Ariel Stoler <amstoler@gmail.com>
2018-01-30 18:23:48 -0500

    Added all getters and setters

--- src/main/java/com/example/demo/Course.java
+++ src/main/java/com/example/demo/Course.java
@@ -31,5 +31,44 @@ public class Course {
     @Min(3)
     private int credit;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
 }
 
